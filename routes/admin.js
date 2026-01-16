c0545ddea125daf815c8af40e96c6e6996e2bd8f
Emerson Junior <emerson.c.junior.115@gmail.com>
2026-01-16 14:48:29 -0300

    feat: enhancing the app

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
-});
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
+
+// /edit-product/:productId => GET
+router.get("/edit-product/:productId", adminController.getEditProduct)
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post("/add-product", adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+// /admin/edit-product => POST
+router.post("/edit-product", adminController.postEditProduct)
+
+module.exports = router;
